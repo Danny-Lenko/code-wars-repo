0e3127cb8451f834523f9e4a7a5abbe09d10c6e2
DannyLenk <danylenko.14@gmail.com>
2022-05-22 12:10:33 +0300

    Make a function that does arithmetic!

---
+++ may-2022/Make a function that does arithmetic!.js
@@ -0,0 +1,8 @@
+"use strict"
+
+function arithmetic(a, b, operator){
+   return operator === 'add' ? a + b
+     : operator === 'subtract' ? a - b
+     : operator === 'divide' ? a / b
+     : a * b
+ }
