738b822decec1393f65df7b25d76eab31f35f7c3
DannyLenk <danylenko.14@gmail.com>
2022-05-26 13:56:24 +0300

    Two Oldest Ages

---
+++ may-2022/Two Oldest Ages.js
@@ -0,0 +1,8 @@
+"use strict"
+
+// return the two oldest/oldest ages within the array of ages passed in.
+function twoOldestAges(ages){
+   ages.sort((a,b)=>b-a)
+   return [ages[1], ages[0]]
+ }
+ 
