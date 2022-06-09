d45f4a3013da2f617810e0483b99194d3a6d5fcf
DannyLenk <danylenko.14@gmail.com>
2022-06-09 15:58:19 +0300

    function checkExam(array1, array2) {

--- june-2022/template.js
+++ june-2022/template.js
@@ -1,10 +1,11 @@
 "use strict"
 
-function roundToNext5(n){
-   if (n % 5 === 0) return n
-   let counter = n
-   while (counter % 5 !== 0) {
-     counter++
-   }
-   return counter
+function checkExam(array1, array2) {
+   array2 = array2.map((answer, i) => (
+     answer === array1[i] ? 4
+     : answer === '' ? 0
+     : -1
+   ))
+   array2 = array2.reduce((a,b)=>a+b,0)
+   return array2 > 0 ? array2 : 0
  }
