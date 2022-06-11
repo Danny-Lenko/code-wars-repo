de14442bff980633479186e2c865c7730456c823
DannyLenk <danylenko.14@gmail.com>
2022-06-11 08:13:03 +0300

    Return the day

---
+++ june-2022/Return the day .js
@@ -0,0 +1,6 @@
+"use strict"
+
+function whatday(num) { 
+   const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+   return days[num-1] ? days[num-1] : 'Wrong, please enter a number between 1 and 7'
+ }
