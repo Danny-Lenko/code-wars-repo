c1e39e610d7d1c35329f8cec7173b6f88fb4cef0
Valerii Danylenko <devdanny.14@gmail.com>
2023-07-20 08:25:46 +0300

    tested the slice shallow copying

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 // spread
-const arr = ['V', 'D']
-console.log(['C', ...arr])
+const arr = ["V", "D"];
+console.log(["C", ...arr]);
 
 // rest
 const log = (...rest) => {
-   console.log(rest)
-}
+  console.log(rest);
+};
 
-log('V', 'D', 'C')
+log("V", "D", "C");
 
 // in vs. hasOwnProperty
 // const test = new Object()
@@ -19,11 +19,35 @@ log('V', 'D', 'C')
 // console.log(test.hasOwnProperty('prop'))
 // console.log(test.hasOwnProperty('toString'))
 
-
 const person = {
-   name: "John"
-}
+  name: "John",
+};
 
-Object.freeze(person)
-person.name = "Jack"
-console.log(person.name)
+Object.freeze(person);
+person.name = "Jack";
+console.log(person.name);
+
+const fruit = [
+  "banana",
+  "grapefruit",
+  "orange",
+  "banana",
+  "grapefruit",
+  "banana",
+];
+
+const cats = [
+   { id: 1, category: "A", name: "Item 1" },
+   { id: 2, category: "B", name: "Item 2" },
+   { id: 3, category: "A", name: "Item 3" },
+   { id: 4, category: "C", name: "Item 4" },
+   { id: 5, category: "B", name: "Item 5" },
+ ];
+
+const sliceCats = cats.slice(1, 4)
+
+sliceCats[0].name = 'Hello'
+
+console.log(cats)
+
+
